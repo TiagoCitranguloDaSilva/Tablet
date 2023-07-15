ed7d45cf3f206c5d48c0ba40db4dbb3d89dbdb66
Tiago Citrangulo da Silva <130724423+TiagoCitranguloDaSilva@users.noreply.github.com>
2023-07-14 21:34:46 -0300

    Começo da segunda parte do projeto

--- js/script.js
+++ js/script.js
@@ -10,45 +10,10 @@ setInterval(function(){
 
 
 
-const ele = document.getElementById('display');
+let iframe = document.getElementById('ifr')
 
-
-let pos = { top: 0, left: 0, x: 0, y: 0 };
-
-const mouseDownHandler = function (e) {
-    ele.style.cursor = 'grabbing';
-    ele.style.userSelect = 'none';
-
-    pos = {
-        left: ele.scrollLeft,
-        top: ele.scrollTop,
-        // Get the current mouse position
-        x: e.clientX,
-        y: e.clientY,
-    };
-
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-
-const mouseMoveHandler = function (e) {
-    // How far the mouse has been moved
-    const dx = e.clientX - pos.x;
-    const dy = e.clientY - pos.y;
-
-    // Scroll the element
-    ele.scrollTop = pos.top - dy;
-    ele.scrollLeft = pos.left - dx;
-};
-
-const mouseUpHandler = function () {
-    ele.style.cursor = 'grab';
-    ele.style.removeProperty('user-select');
-
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-    ele.style.cursor = 'default'
-};
-
-// Attach the handler
-ele.addEventListener('mousedown', mouseDownHandler);
+function mudar(Link){
+    Link = (Link == 'Google') ? "https://www.google.com/" : ''
+    alert(Link)
+    iframe.src = Link
+}
