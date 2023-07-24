8ab0d2cfc73221c21c65c21717dc809a735ebe14
Tiago Citrangulo da Silva <130724423+TiagoCitranguloDaSilva@users.noreply.github.com>
2023-07-24 13:20:09 -0300

    Fazendo funcionar

--- js/script.js
+++ js/script.js
@@ -7,13 +7,23 @@ setInterval(function(){
 }, 1000)
 
 
-
-
-
 let iframe = document.getElementById('ifr')
+let tela = document.getElementById('display')
+let btnSair = document.getElementById('x')
+
+
 
 function mudar(Link){
-    Link = (Link == 'Google') ? "https://www.google.com/" : ''
-    alert(Link)
-    iframe.src = Link
+
+    iframe.src = 'https://'+ Link
+    iframe.style.display = 'block'
+    tela.style.display = 'none'
+    btnSair.style.display = 'block'
+}
+
+function sair(){
+    iframe.src = ''
+    iframe.style.display = 'none'
+    tela.style.display = 'grid'
+    btnSair.style.display = 'none'
 }
